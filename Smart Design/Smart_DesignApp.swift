1387b502f60280c4217307474b0fef91b62dba23
nurlandadashbayli <nurlan.dadashbayli@hsrw.org>
2022-11-17 22:52:06 +0100

    Initial Commit

---
+++ Smart Design/Smart_DesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_DesignApp.swift
+//  Smart Design
+//
+//  Created by Nurlan Dadashbayli on 17.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Smart_DesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
